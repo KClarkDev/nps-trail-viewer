5505161b6e41dd3557e9cf336a2df2d099615d6c
Katherine Clark <60265279+KClarkDev@users.noreply.github.com>
2023-10-25 19:46:30 -0400

    Added feature layer to ArcGISMap component

--- client/src/components/ArcGISMap.jsx
+++ client/src/components/ArcGISMap.jsx
@@ -5,22 +5,30 @@ const ArcGISMap = () => {
   const mapDiv = useRef();
 
   useEffect(() => {
-    loadModules(["esri/Map", "esri/views/SceneView"], { css: true }).then(
-      ([Map, SceneView]) => {
-        // Create a new map and scene view
-        const map = new Map({
-          basemap: "topo",
-          ground: "world-elevation",
-        });
+    loadModules(
+      ["esri/Map", "esri/views/SceneView", "esri/layers/FeatureLayer"],
+      { css: true }
+    ).then(([Map, SceneView, FeatureLayer]) => {
+      // Create a new map and scene view
+      const map = new Map({
+        basemap: "topo",
+        ground: "world-elevation",
+      });
 
-        const view = new SceneView({
-          container: mapDiv.current,
-          map: map,
-          zoom: 10,
-          center: [-78.45, 38.47], // long, lat (Shenandoah National Park)
-        });
-      }
-    );
+      const view = new SceneView({
+        container: mapDiv.current,
+        map: map,
+        zoom: 10,
+        center: [-78.45, 38.47], // long, lat (Shenandoah National Park)
+      });
+      // Create a feature layer and specify the URL of your feature service
+      const featureLayer = new FeatureLayer({
+        url: "https://services6.arcgis.com/cGI8zn9Oo7U9dF6z/arcgis/rest/services/Shenandoah_National_Park_Boundary/FeatureServer",
+      });
+
+      // Add the feature layer to the map
+      map.add(featureLayer);
+    });
   }, []);
 
   return (
