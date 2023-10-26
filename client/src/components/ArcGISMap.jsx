60c377a00deb3c3099816fc54d6f60bb5bb8f8a6
Katherine Clark <60265279+KClarkDev@users.noreply.github.com>
2023-10-26 19:40:58 -0400

    Added menu panel component and position styling. Also added trails layer to map

--- client/src/components/ArcGISMap.jsx
+++ client/src/components/ArcGISMap.jsx
@@ -1,5 +1,7 @@
 import { useEffect, useRef } from "react";
 import { loadModules } from "esri-loader";
+import MenuPanel from "./MenuPanel";
+import "../styles/menuPanel.css";
 
 const ArcGISMap = () => {
   const mapDiv = useRef();
@@ -21,19 +23,36 @@ const ArcGISMap = () => {
         zoom: 10,
         center: [-78.45, 38.47], // long, lat (Shenandoah National Park)
       });
-      // Create a feature layer and specify the URL of your feature service
-      const featureLayer = new FeatureLayer({
+
+      const shenandoahBoundary = new FeatureLayer({
         url: "https://services6.arcgis.com/cGI8zn9Oo7U9dF6z/arcgis/rest/services/Shenandoah_National_Park_Boundary/FeatureServer",
       });
 
+      const shenandoahTrails = new FeatureLayer({
+        url: "https://services8.arcgis.com/ppeEwsORWhtYmSAw/arcgis/rest/services/Shenandoah_National_Park_Trails/FeatureServer",
+      });
+
       // Add the feature layer to the map
-      map.add(featureLayer);
+      map.add(shenandoahBoundary);
+      map.add(shenandoahTrails);
     });
   }, []);
 
   return (
-    <div ref={mapDiv} style={{ width: "100%", height: "100vh" }}>
-      {/* Map container */}
+    <div>
+      <MenuPanel />
+      <div
+        ref={mapDiv}
+        // Position map container below the navbar
+        style={{
+          width: "100%",
+          height: "100vh",
+          position: "fixed",
+          top: "65px",
+        }}
+      >
+        {/* Map container */}
+      </div>
     </div>
   );
 };
---
+++ client/src/components/MenuPanel.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "../styles/menuPanel.css";
+
+const MenuPanel = () => {
+  // h1 section, title
+  //
+  return (
+    <div className="menu-panel">
+      <h2>Menu Panel</h2>
+      <ul>
+        <li>Menu Item 1</li>
+        <li>Menu Item 2</li>
+        <li>Menu Item 3</li>
+      </ul>
+    </div>
+  );
+};
+
+export default MenuPanel;
